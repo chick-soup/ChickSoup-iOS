117d144e41442b22ccda731439b73d2431d5db11
DohyunKIm <rlaehgus1229@gmail.com>
2019-12-16 23:35:31 +0900

    Add MyProfileVC

---
+++ Kakaotalk_CloneCoding/MyProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MyProfileViewController.swift
+//  Kakaotalk_CloneCoding
+//
+//  Created by DohyunKim on 16/12/2019.
+//  Copyright © 2019 DohyunKim. All rights reserved.
+//
+
+import UIKit
+
+class MyProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
