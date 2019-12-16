0fb84a8d469758845d4f3f0a78448177251f1cb8
DohyunKIm <rlaehgus1229@gmail.com>
2019-12-16 23:43:19 +0900

    Seperate RefreshFunc to another SwiftFile

---
+++ Kakaotalk_CloneCoding/FuncRefresh.swift
@@ -0,0 +1,51 @@
+//
+//  FuncRefresh.swift
+//  Kakaotalk_CloneCoding
+//
+//  Created by DohyunKim on 16/12/2019.
+//  Copyright © 2019 DohyunKim. All rights reserved.
+//
+
+import Foundation
+
+let refreshUrl = URL(string: "https://chicksoup.s3.ap-northeast-2.amazonaws.com/refresh")
+
+func tokenRefresh() {
+        
+        var request = URLRequest(url: refreshUrl!)
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "access_token")
+        request.addValue("Authorization", forHTTPHeaderField: String(UserDefaults.standard.string(forKey :"refresh_token")!))
+        
+        request.httpMethod = "GET"
+        URLSession.shared.dataTask(with: request){
+            [weak self] data, res, err in
+            guard self != nil else { return }
+            if let err = err { print(err.localizedDescription); return }
+            print((res as! HTTPURLResponse).statusCode)
+            switch (res as! HTTPURLResponse).statusCode{
+            case 200:
+                let jsonSerialization = try! JSONSerialization.jsonObject(with: data!, options: []) as! [String:Any]
+                print(jsonSerialization)
+                
+                UserDefaults.standard.set(jsonSerialization["access_token"], forKey: "access_token")
+                
+            case 401:
+                print("request의 header에 Authorization으로 JWT를 포함하지 않았거나 빈 문자열을 줌")
+                
+//            case 403:
+//                print("사용 가능 기간이 만료된 JWT")
+                
+            case 404:
+                print("JWT로 인증된 사용자가 실제로는 존재하지 않음")
+                
+            case 422:
+                print("서버에서 해석할 수 없는 잘못된 형식의 JWT or 다른 타입의 토큰을 넘겨 줌")
+                
+            default:
+                print("error")
+            }
+        }.resume()
+    }
